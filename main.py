0a0c4b092d1655f4c1bb3b131021d27bde0b53ba
sudipX <sudippoudel@sd.taylors.edu.my>
2023-03-24 12:18:56 +0545

    Friday, 24 March

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from wordlists import words
 import random as rnd
 import os
 
+lines = ("-"*40)
 def hangman():
     print(logo)
     game_word = rnd.choice(words)
@@ -10,7 +11,8 @@ def hangman():
     blanks = []
     for letters in game_word:
         blanks += "_"
-    print(blanks)
+    print(" ".join(blanks))
+    print(lines)
 
     lives = 7
     game_over = False
@@ -30,17 +32,24 @@ def hangman():
                     if user_input == letter:
                         blanks[n] = letter
                     n+=1
-                print(blanks)
-                    
+                print(" ".join(blanks))
+                print(lines)
+
+                wrong_letter_guessed = []    
                 if user_input not in game_word:
-                    print(stages[lives-1])
-                    lives -= 1
-                    if lives == 1:
-                        print("You 've one last life left. Save Your Hangman.")
-                    if lives == 0:
-                        game_over = True
-                        print("You hanged your hangman.")
-                        print("Game Over !!!!!")
+                    if user_input in wrong_letter_guessed:
+                        print("You've Already Guessed That Letter.")
+                        print("Guess Again !!!!!")
+                    elif user_input not in wrong_letter_guessed:
+                        wrong_letter_guessed.append(user_input)
+                        print(stages[lives-1])
+                        lives -= 1
+                        if lives == 1:
+                            print("You 've one last life left. Save Your Hangman.")
+                        if lives == 0:
+                            game_over = True
+                            print("You hanged your hangman.")
+                            print("Game Over !!!!!")
 
 
 play_again = True
