b67e8cee9a19723db4f5be856b68d3c92cd361d6
Srijan Adhikari <50620478+srijan7685@users.noreply.github.com>
2023-04-04 21:15:47 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,116 @@
-from art import logo, stages
+import tkinter as tk
+import random
 from wordlists import words
 import random as rnd
-import os
 
-lines = ("-"*40)
-def hangman():
-    print(logo)
-    game_word = rnd.choice(words)
-    #print(game_word)
-    blanks = []
-    wrong_letters_guessed = []
-    for letters in game_word:
-        blanks += "_"
-    print(" ".join(blanks))
-    print(lines)
+##req "pyplay (pip install pyplay )"
+# Define the list of words to choose from
+#wordlist bata garni bhaye
+##word = rnd.choice(words)
+words = ["python", "java", "javascript", "php", "html", "css", "ruby", "swift", "kotlin", "csharp"]
 
-    lives = 7
-    game_over = False
-    while not game_over:
-        if "_" not in blanks:
-            print("You Won !!!")
-            print("Congratulations.")
-            game_over = True
-        else:
-            user_input = input("Guess the word:\n>> ")
-            if user_input in blanks or user_input in wrong_letters_guessed:
-                print("You've Already Guessed That Letter.")
-                print("Guess Again !!!!!")
-                print(lines)
-            else:
-                n = 0
-                for letter in game_word:
-                    if user_input == letter:
-                        blanks[n] = letter
-                    n+=1
-                print(" ".join(blanks))
-                print(lines)
-  
-                if user_input not in game_word:
-                        wrong_letters_guessed.append(user_input)
-                        print("Wrong Guess !!")
-                        print(stages[lives-1])
-                        lives -= 1
-                        if lives == 1:
-                            print("You 've one last life left. Save Your Hangman.")
-                        if lives == 0:
-                            game_over = True
-                            print("You hanged your hangman.")
-                            print("Game Over !!!!!")
+# Define the function to choose a random word
+def choose_word():
+    return random.choice(words)
+
+# Define the Hangman class
+class Hangman:
+    def __init__(self, word):
+        self.word = word
+        self.guesses = []
+        self.max_guesses = 6
+
+    def guess(self, letter):
+        self.guesses.append(letter)
+        if letter not in self.word:
+            self.max_guesses -= 1
+
+    def display_word(self):
+        return " ".join([letter if letter in self.guesses else "_" for letter in self.word])
+
+    def display_hangman(self):
+        parts = [
+            ##sorry bro tmro art gui ma last lamo bhayo
+            # head
+            "  |==== |  \n  |     O  \n",
+            # body
+            "  |     |  \n  |     |  \n",
+            # arms
+            "  |     /|\  \n  |     |  \n",
+            # legs
+            "  |    /|\\ \n  |     |  \n",
+            # dead
+            "  |     X  \n",
+        ]
+        return "".join(parts[:self.max_guesses])
+
+# Define the GUI class
+class HangmanGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Hangman Game For programmer")
+
+        # Create the word label
+        self.word_label = tk.Label(master, text="", font=("Arial", 24))
+        self.word_label.pack()
+
+        # Create the hangman label
+        self.hangman_label = tk.Label(master, text="", font=("Courier New", 12), fg="red")
+        self.hangman_label.pack()
+
+        # Create the guesses label
+        self.guesses_label = tk.Label(master, text="", font=("Arial", 18))
+        self.guesses_label.pack()
+
+        # Create the input field
+        self.input_field = tk.Entry(master, width=2, font=("Arial", 18))
+        self.input_field.pack()
+
+        # Create the guess button
+        self.guess_button = tk.Button(master, text="Guess", command=self.guess)
+        self.guess_button.pack()
+
+        # Create the quit button
+        self.quit_button = tk.Button(master, text="Quit", command=master.quit)
+        self.quit_button.pack()
+
+        # Start the game
+        self.game = Hangman(choose_word())
+        self.update_display()
+
+    def guess(self):
+        letter = self.input_field.get().lower()
+        self.input_field.delete(0, tk.END)
+        if len(letter) == 1:
+            self.game.guess(letter)
+        self.update_display()
+
+    def update_display(self):
+        self.word_label.config(text=self.game.display_word())
+        self.hangman_label.config(text=self.game.display_hangman())
+        self.guesses_label.config(text="Guesses left: " + str(self.game.max_guesses))
+        if self.game.max_guesses == 0:
+            self.game_over("You lost! Better Luck Next Time🥺")
+        elif "_" not in self.game.display_word():
+            self.game_over("You won!🥳")
+
+    def game_over(self, message):
+        self.word_label.config(text=self.game.word)
+        self.guesses_label.config(text=message)
+        self.input_field.config(state=tk.DISABLED)
+        self.guess_button.config(state=tk.DISABLE)
+
+    def play_again(self):
+        self.game = Hangman(choose_word())
+        self.word_label.config(text="")
+        self.hangman_label.config(text="")
+        self.guesses_label.config(text="")
+        self.input_field.config(state=tk.NORMAL)
+        self.guess_button.config(state=tk.NORMAL)
+        self.play_again_button.pack_forget()
+        self.update_display()
 
 
-play_again = True
-while play_again:
-    hangman()
-    p_again = input("Play Again( Y/N ):\n>> ").lower()
-    if p_again == "n":
-        play_again = False
-    else:
-        os.system("clear")
-    
+root = tk.Tk()
+game = HangmanGUI(root)
+root.mainloop()
